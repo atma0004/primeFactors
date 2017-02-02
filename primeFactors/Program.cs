f591ab0b27f5b18415bdf5901c25b668a231a1e6
atma0004 <atma0004@edu.eal.dk>
2017-02-02 12:41:00 +0100

    Add project files.

---
+++ primeFactors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeFactors
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            long a = 600851475143;
+            long ta = 0;
+            for (long i = 2; i<=a;)
+                {
+                if (a%i==0)
+                    {
+                    a/=i;
+                    ta=i;
+                    }
+                else
+                    i++;
+                }
+            Console.Write(ta.ToString());
+            Console.ReadLine();
+            }
+        }
+    }
